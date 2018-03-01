fe3e1c439bdf33c1e2f8fd945fe7cbc5862c8ed6
C3NZ <epichdd@gmail.com>
2018-02-28 23:39:34 -0500

    Linked bot to database with minimal functionality (Logs commands into table if the command is valid) and added a few more basic commands

--- phi.py
+++ phi.py
@@ -1,6 +1,8 @@
 import discord
 import asyncio
 import config
+import db
+
 
 client = discord.Client();
 
@@ -13,8 +15,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+	valid_command = True
+
 	if message.content.startswith('$phi'):
 		await client.send_message(message.channel, 'Hey, that\'s me!')
+	elif message.content.startswith('$cultured'):
+		await client.send_message(message.channel, 'Ah, yes, our lord and savior, @cenz#4867')
+	elif message.content.startswith('$goodboy'):
+		await client.send_message(message.channel, 'Woof!')
+	else:
+		valid_command = False
+		
+	if valid_command:
+		user_input = message.content.split()
+		db.add_command_to_history(user_input[0], user_input[1:], message.author.name)
 
-
-client.run(config.DISCORD_TOKEN)
+if __name__ == '__main__':
+	db.create_db_connection()
+	client.run(config.DISCORD_TOKEN)
