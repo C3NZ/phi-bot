d82fb8de3606948cc05fae068059f9c45a0dd179
C3NZ <epichdd@gmail.com>
2018-03-04 23:17:08 -0500

    Added a simple wrapper function for printing to the console, and fixed helper functions

--- phi.py
+++ phi.py
@@ -6,21 +6,29 @@ import db
 #Discord client for interacting with users
 client = discord.Client();
 
+#Very simple print wrapper for not having to write if statements all throughout the code
+def log_to_console(string):
+	if config.DEBUG_MODE:
+		print(string)
+
 #------Helper functions------
+
+#Hey, that's me!
 async def thats_me(message):
 	await client.send_message(message.channel, 'Hey, that\'s me!')
 
 #Process a users bank account
 async def process_bank_account(command_input, message):
-	if len(chopped_input) <= 1:
-			print(chopped_input)
+	
+	if len(command_input) == 1:
+			print(command_input)
 			await client.send_message(message.channel, 'This is where your bank info will be!')
 	else:
-		print(chopped_input)
-		
-		if chopped_input[1] == 'start':
+		if command_input[1] == 'start':
 			await client.send_message(message.channel, 'Starting your bank account!')
 
+
+#-----Handling client events-----
 @client.event
 async def on_ready():
 	print('logged in as')
@@ -33,25 +41,27 @@ async def on_message(message):
 	valid_command = True
 
 	if message.content.startswith('$phi'):
-		thats_me(message)
+		await thats_me(message)
 	elif message.content.startswith('$cultured'):
 		await client.send_message(message.channel, 'Ah, yes, our lord and savior, @cenz#4867')
 	elif message.content.startswith('$goodboy'):
 		await client.send_message(message.channel, 'Woof!')
 	elif message.content.startswith('$bank'):
-		chopped_input = message.content.split()
-		process_bank_account(chopped_input, message)
+		command_input = message.content.split()
+		await process_bank_account(command_input, message)
 	elif message.content.startswith('$yt'):
 		await client.send_message(message.channel, '?play https://www.youtube.com/watch?v=4yym5Og_Oeg')
 	else:
 		valid_command = False
 
+	#Log command to database
 	if valid_command:
 		user_input = message.content.split()
 		db.add_command_to_history(user_input[0], user_input[1:], message.author.name)
 
+
 def shutdown():
-	print('Shutting down the bot now')
+	log_to_console('shutting down now')
 
 if __name__ == '__main__':
 	db.create_db_connection()
